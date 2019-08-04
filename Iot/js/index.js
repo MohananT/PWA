045487154073d5927b48396df49b7a0814ad6877
MohananT <mohan.stanky@gmail.com>
2019-08-04 22:08:49 +0530

    -add

--- Iot/js/index.js
+++ Iot/js/index.js
@@ -15,10 +15,46 @@ _getDBContents().then(res => {
   console.log("selector", selector);
 
   items.forEach(item => {
-    // let div = document.createElement('div');
-    // div.
-  });
+    let div = document.createElement("div");
+    div.innerHTML = `<div class="col s12 col m4 box">
+    <div class="row">
+      <div class="col s6">
+        <b>Reservoir :</b>
+      </div>
+      <div class="col s6">
+        <i>${item.ReservoirName}</i>
+      </div>
+    </div>
 
+    <div class="row">
+      <div class="col s6">
+        <b>Capacity (liters):</b>
+      </div>
+      <div class="col s6">
+        <i>${item.Capacity}</i>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col s6">
+        <b>Location :</b>
+      </div>
+      <div class="col s6">
+        <i>${item.Location}</i>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col s6">
+        <b>Current Status (%):</b>
+      </div>
+      <div class="col s6">
+        <i>${item.StatusValue}</i>
+      </div>
+    </div>
+  </div>`;
+    selector.appendChild(div);
+  });
 });
 
 $(document).ready(function() {
